751c058484751616b2b16f216cf31f4f649b2d9d
ALEXANDRA-GABRIELA GRĂDINARU <alexandra.gradinaru@stud.ubbcluj.ro>
2021-10-14 20:00:45 +0300

    Add project files.

---
+++ Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gradinaru_Alexandra_Lab2.Models
+{
+    public class Customer
+    {
+        //permite introducerea cheii primare 
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CustomerID { get; set; }
+        public String Name { get; set; }
+        public String Adress { get; set; }
+        public DateTime BirthDate { get; set; }
+        public ICollection<Order> Orders { get; set; }
+        //relationarea cu proprietatea Order
+    }
+}
